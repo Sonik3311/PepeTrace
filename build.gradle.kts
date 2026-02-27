ad393cc6ad8558064c9ebb2bba679f60cccf8928
Sonik3311 <darkissoul2017@gmail.com>
2026-02-27 16:03:08 +0300

    Добавил LWJGL зависимость

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,27 @@
+import org.gradle.internal.os.OperatingSystem
+
+val lwjglVersion by extra("3.4.1")
+
+val lwjglNatives: String by extra {
+    when (OperatingSystem.current()) {
+        OperatingSystem.LINUX -> {
+            val base = "natives-linux"
+            val osArch = System.getProperty("os.arch")
+            when {
+                osArch.startsWith("arm") || osArch.startsWith("aarch64") -> {
+                    base + if (osArch.contains("64") || osArch.startsWith("armv8")) "-arm64" else "-arm32"
+                }
+                osArch.startsWith("ppc") -> "$base-ppc64le"
+                osArch.startsWith("riscv") -> "$base-riscv64"
+                else -> base
+            }
+        }
+        OperatingSystem.WINDOWS -> "natives-windows"
+        else -> error("Unsupported operating system")
+    }
+}
+
+
 plugins {
     id("java")
 }
@@ -13,8 +37,26 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+
+	implementation("org.lwjgl:lwjgl")
+	implementation("org.lwjgl:lwjgl-assimp")
+	implementation("org.lwjgl:lwjgl-glfw")
+	implementation("org.lwjgl:lwjgl-opengl")
+	implementation("org.lwjgl:lwjgl-stb")
+	implementation("org.lwjgl:lwjgl::$lwjglNatives")
+	implementation("org.lwjgl:lwjgl-assimp::$lwjglNatives")
+	implementation("org.lwjgl:lwjgl-glfw::$lwjglNatives")
+	implementation("org.lwjgl:lwjgl-opengl::$lwjglNatives")
+	implementation("org.lwjgl:lwjgl-stb::$lwjglNatives")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+repositories {
+	mavenCentral()
 }
