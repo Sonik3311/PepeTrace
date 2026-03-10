f0a5c149710e8b7b166a47e3a8cce739fc60c7f5
Sonik3311 <darkissoul2017@gmail.com>
2026-03-10 16:04:36 +0300

    Организация

---
+++ src/main/glsl/util/intersections.glsl
@@ -0,0 +1,42 @@
+struct HitResult {
+    bool isValid;
+    vec3 position;
+    float distance;
+    vec3 normal;
+};
+
+HitResult triIntersect(vec3 ro, vec3 rd, vec3 A, vec3 B, vec3 C) {
+    vec3 edgeAB = B - A;
+    vec3 edgeAC = C - A;
+    vec3 h = cross(rd, edgeAC);
+    float det = dot(edgeAB, h);
+
+    HitResult hit_result;
+    hit_result.isValid = false;
+    hit_result.position = vec3(0);
+    hit_result.normal = vec3(0);
+    hit_result.distance = -1;
+
+    if (det < EPSILON) return hit_result;
+    float invDet = 1.0 / det;
+
+    vec3 s = ro - A;
+    float u = dot(s, h) * invDet;
+
+    if (u < 0 || u > 1) return hit_result;
+
+    vec3 q = cross(s, edgeAB);
+    float v = dot(rd, q) * invDet;
+
+    if (v < 0.1 || u + v > 0.9) return hit_result;
+
+    // Calculate t (distance along ray)
+    float t = dot(edgeAC, q) * invDet;
+
+    hit_result.isValid = true;
+    hit_result.position = ro + rd * t;
+    hit_result.distance = t;
+    hit_result.normal = cross(edgeAB, edgeAC);
+
+    return hit_result;
+}
