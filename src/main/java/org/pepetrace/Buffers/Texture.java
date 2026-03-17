25a074ceaf7cb88271bf8a8c1f955675978e4291
Sonik3311 <darkissoul2017@gmail.com>
2026-03-17 19:07:02 +0300

    TODO в Texture

--- src/main/java/org/pepetrace/Buffers/Texture.java
+++ src/main/java/org/pepetrace/Buffers/Texture.java
@@ -13,6 +13,10 @@ public class Texture {
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+        //TODO: NULL здесь нужно заменить на картинку,
+        // полученную через org.lwjgl.stb.STBImage.stbi_load
+        // Тогда можно будет загружать картинки на видеокарту
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, width, height, 0,pixel_format,
                 pixel_datatype, NULL);
 
